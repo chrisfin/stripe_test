5e1e6e491a6cd201e7b3aa2ca45f76d0335a0607
chrisfin <chrisfinlayson@gmail.com>
2018-04-02 09:09:25 -0700

    Implement Stripe Elements

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => 'pk_test_3ODGlBu89xjDT3uQizYeiyUU',
+  :secret_key      => 'sk_test_tJERwH4SJdLGTDOyR349tse3'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
