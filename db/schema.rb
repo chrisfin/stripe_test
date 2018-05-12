fd206479527fa91b49dbfabb5d7d063ef2311d5a
chrisfin <chrisfinlayson@gmail.com>
2018-05-11 19:37:03 -0700

    Add Editors remove Checkout'

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329013120) do
+ActiveRecord::Schema.define(version: 20180512022136) do
 
   create_table "charges", force: :cascade do |t|
     t.string "token"
     t.float "amount"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "editors", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
