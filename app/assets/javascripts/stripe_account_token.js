c1191364e8ba09cf46b372e3f50db1e10d47cd18
chrisfin <chrisfinlayson@gmail.com>
2018-05-27 19:27:18 -0700

    Finish Connect Account complete

--- app/assets/javascripts/stripe_account_token.js
+++ app/assets/javascripts/stripe_account_token.js
@@ -10,13 +10,14 @@ async function handleForm(event) {
   
   var account_token = await stripe.createToken('account', {
     legal_entity: {
+      ssn_last_4: document.querySelector('.inp-ssn_last_4').value,
       first_name: document.querySelector('.inp-first-name').value,
       last_name: document.querySelector('.inp-last-name').value,
       address: {
         line1: document.querySelector('.inp-street-address1').value,
         city: document.querySelector('.inp-city').value,
         state: document.querySelector('.inp-state').value,
-        postal_code: document.querySelector('.inp-zip').value,
+        postal_code: document.querySelector('.inp-zip').value, 
       },
     },
     tos_shown_and_accepted: true,
